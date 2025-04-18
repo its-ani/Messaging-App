a4a0f3bb4b86d87fa472cf19604e482a49543706
its-ani <guptaanirudh712@gmail.com>
2025-04-18 18:38:43 +0530

    Docker

---
+++ WhatsAppClone/src/main/java/com/anirudh/WhatsAppClone/WhatsAppCloneApplication.java
@@ -0,0 +1,13 @@
+package com.anirudh.WhatsAppClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppCloneApplication.class, args);
+	}
+
+}
---
+++ WhatsAppClone/src/test/java/com/anirudh/WhatsAppClone/WhatsAppCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anirudh.WhatsAppClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
