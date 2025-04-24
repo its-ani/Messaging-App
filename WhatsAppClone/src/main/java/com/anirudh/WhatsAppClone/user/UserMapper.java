d396bd15eb02e4ea7e6c4c16ca4a9e83c88efdf3
its-ani <guptaanirudh712@gmail.com>
2025-04-24 16:32:09 +0530

    UsersynchronizerFIlter

---
+++ WhatsAppClone/src/main/java/com/anirudh/WhatsAppClone/user/UserMapper.java
@@ -0,0 +1,38 @@
+package com.anirudh.WhatsAppClone.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserMapper {
+    public User fromTokenAttributes(Map<String, Object> attributes){
+        User user = new User();
+
+        if(attributes.containsKey("sub")){
+            user.setId(attributes.get("sub").toString());
+        }
+
+        if(attributes.containsKey("given_name")){
+            user.setFirstName(attributes.get("name").toString());
+        }
+        else if(attributes.containsKey("nickname")){
+            user.setFirstName(attributes.get("nickname").toString());
+        }
+
+        if (attributes.containsKey("family_name")) {
+            user.setLastName(attributes.get("family_name").toString());
+        }
+
+        if (attributes.containsKey("email")) {
+            user.setEmail(attributes.get("email").toString());
+        }
+        user.setLastSeen(LocalDateTime.now());
+        return user;
+    }
+}
---
+++ WhatsAppClone/src/main/java/com/anirudh/WhatsAppClone/user/UserSynchronizer.java
@@ -0,0 +1,42 @@
+package com.anirudh.WhatsAppClone.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+
+public class UserSynchronizer {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+
+    public void synchronizedWithIdp(Jwt token) {
+        log.info("Synchronizing user with idp");
+        getUserEmail(token).ifPresent(userEmail -> {
+            log.info("Synchronizing user with email {}", userEmail);
+//            Optional<User> optUser = userRepository.findByEmail(userEmail);
+            User user = userMapper.fromTokenAttributes(token.getClaims());
+//            optUser.ifPresent(value -> user.setId(optUser.get().getId()));
+
+            userRepository.save(user);
+        });
+    }
+
+    private Optional<String> getUserEmail(Jwt token) {
+        Map<String, Object> attributes = token.getClaims();
+        if(attributes.containsKey("email")) {
+            return Optional.of(attributes.get("email").toString());
+        }
+
+        return Optional.empty();
+    }
+}
